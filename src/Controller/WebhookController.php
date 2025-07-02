2a4512193b85dde695172e2f938c8188c14caa2e
Samuel Etheve <samuel@pc-1524.home>
2025-07-02 15:58:32 +0400

    test

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -17,14 +17,22 @@ final class WebhookController extends AbstractController
         $this->projectDir = $projectDir;
     }
 
-    #[Route('/webhook', name: 'app_webhook', methods: ['POST', 'HEAD'])]
+    #[Route('/webhook', name: 'app_webhook', methods: ['GET', 'POST', 'HEAD'])]
     public function index(Request $request): Response
     {
-        $data = json_decode($request->getContent(), true);
+        if ($request->getMethod() === 'HEAD') {
+            return new Response('OK', 200);
+        }
 
+        if ($request->getMethod() === 'GET') {
+            return new Response('Webhook en ligne ✅ (GET)', 200);
+        }
+
+        $data = json_decode($request->getContent(), true);
         file_put_contents($this->projectDir . '/DEBUG/trello_webhook.log', print_r($data, true), FILE_APPEND);
 
         return new Response('OK', 200);
     }
+
 
 }
