7165e77abb75071b13548e3f10a8f2db362a57e1
Samuel Etheve <samuel@pc-1524.home>
2025-07-02 16:05:40 +0400

    test

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -29,7 +29,25 @@ final class WebhookController extends AbstractController
         }
 
         $data = json_decode($request->getContent(), true);
-        file_put_contents($this->projectDir . '/DEBUG/trello_webhook.log', print_r($data, true), FILE_APPEND);
+
+        if (
+            isset($data['action']['type']) &&
+            $data['action']['type'] === 'updateCard' &&
+            isset($data['action']['data']['listBefore'], $data['action']['data']['listAfter'])
+        ) {
+            $cardName = $data['action']['data']['card']['name'] ?? 'Carte inconnue';
+            $fromList = $data['action']['data']['listBefore']['name'] ?? 'Inconnue';
+            $toList = $data['action']['data']['listAfter']['name'] ?? 'Inconnue';
+            $movedBy = $data['action']['memberCreator']['fullName'] ?? 'Quelqu’un';
+            $timestamp = $data['action']['date'] ?? date('c');
+
+            $log = sprintf("[%s] 📦 %s a déplacé \"%s\" de [%s] vers [%s]\n", $timestamp, $movedBy, $cardName, $fromList, $toList);
+
+            file_put_contents($this->projectDir . '/DEBUG/trello_webhook.log', $log, FILE_APPEND);
+        }
+
+        // $data = json_decode($request->getContent(), true);
+        // file_put_contents($this->projectDir . '/DEBUG/trello_webhook.log', print_r($data, true), FILE_APPEND);
 
         return new Response('OK', 200);
     }
